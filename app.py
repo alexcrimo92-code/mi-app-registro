774f97346943c3e2f9fd8f0ea680e9f7983435ea
alexcrimo92-code <alex.cri.mo.92@gmail.com>
2026-06-25 20:38:55 +0200

    Add files via upload
    Primera subida

---
+++ app.py
@@ -0,0 +1,15 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Mi App de Registro"
+    entrada = ft.TextField(label="Escribe algo...", width=300)
+    lista = ft.Column()
+
+    def enviar(e):
+        lista.controls.append(ft.Text(f"- {entrada.value}"))
+        entrada.value = ""
+        page.update()
+
+    page.add(entrada, ft.ElevatedButton("Enviar", on_click=enviar), lista)
+
+ft.app(target=main)
